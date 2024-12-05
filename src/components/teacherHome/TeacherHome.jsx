cee5261a75eb7e2c18b6d211c88f66131d762867
nasser <nelbacha@uade.edu.ar>
2024-12-05 19:02:52 -0300

    fix some details

--- src/components/teacherHome/TeacherHome.jsx
+++ src/components/teacherHome/TeacherHome.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import left from '../../assets/icons/corchete izquierdo.svg';
 import right from '../../assets/icons/corchete derecho.svg';
+import { getIntoSchool } from '../../features/teacher/teacherSlice';
+import { useDispatch } from 'react-redux';
+import GenericDialog from '../common/dialog/dialog';
 
 const TeacherHome = () => {
+  const dispatch = useDispatch();
   const { teacherSchools } = useSelector((state) => state.teacher);
-  const navigate = useNavigate();
+  const [showDialog, setShowDialog] = useState(false);
+  const [code, setCode] = useState('');
 
-  const handleAddSchool = () => {
-    navigate('/teacher/new-school');
-  };
+  const navigate = useNavigate();
 
   const handleSchoolClick = (schoolId) => {
     navigate(`/courses/${schoolId}`);
   };
 
+  const handleJoin = async () => {
+    await dispatch(getIntoSchool(code));
+    setCode('');
+    setShowDialog(false);
+  }
+  
+
   return (
     <div className="flex flex-col items-center min-h-screen bg-grayBg">
-      <header className="flex">
+      <header className="flex mt-20 gap-10">
         <img src={left} alt="Corchete izquierdo" className="h-10 mt-2" />
         <h1 className="text-5xl text-gray-800 mb-12">
           Selecciona una institución
@@ -34,29 +44,45 @@ const TeacherHome = () => {
             style={{borderRadius: '1.5rem'}}
             onClick={() => handleSchoolClick(school.id)}
           >
-            <span className="text-4xl font-semibold block mb-4">{school.name}</span>
-            <div className="flex-inline items-center text-lg">
-              <span className="mr-2">􀎟 3 aulas</span> 
-            </div>
-            <div className="absolute bottom-4 right-4 bg-[#E1E7F8] text-blue-600 rounded-full w-10 h-10 flex items-center justify-center shadow-md hover:shadow-lg">
-              <span className='font-medium text-3xl text-[#4558C8]'> ➔</span>
+            <span className="text-4xl font-semibold block mb-10">{school.name}</span>
+            <div className="flex justify-between">
+              <img src={school.logo} alt="Logo escuela" className='rounded-full w-12 h-12' />
+              <div className="bottom-4 right-4 bg-[#E1E7F8] text-blue-600 rounded-full w-10 h-10 flex items-center justify-center shadow-md hover:shadow-lg">
+                <span className='font-medium text-3xl text-[#4558C8]'> ➔</span>
+              </div>
             </div>
           </div>
         ))}
 
     <div
       className="relative flex flex-col justify-between bg-[#F2F4FC] text-[#4558C8] rounded-lg shadow-[0_8px_0px_0px_rgba(202,213,243,1)] p-6 hover:shadow-[0_12px_0px_0px_rgba(202,213,243,1)] transition-all cursor-pointer"
-      onClick={handleAddSchool}
+      onClick={() => setShowDialog(true)}
     >
       <div>
-        <span className="text-4xl font-bold block mb-4">Cargar nueva</span>
-        <span className="text-lg font-medium text-[#4558C8]">Añade una nueva institución</span>
-      </div>
-      <div className="absolute bottom-3 right-3 bg-darkBlue text-white rounded-full w-10 h-10 flex items-center justify-center shadow-md">
-        <span className='font-medium text-3xl'>+</span>
+        <span className="text-4xl font-bold block mb-10">Cargar nueva</span>
+        <div className="flex justify-between">
+          <span className="text-lg font-medium text-[#4558C8]">Añade una nueva institución</span>
+          <div className="bottom-3 right-3 bg-darkBlue text-white rounded-full w-10 h-10 flex items-center justify-center shadow-md">
+            <span className='font-medium text-3xl' style={{marginTop:'-5px'}}>+</span>
+          </div>
+        </div>
       </div>
     </div>  
     </div>
+    <div>
+        {showDialog &&
+          <GenericDialog 
+          type="form"
+          title={"Ingresar a una institución"}
+          description={"Ingresa el codigo de la institución a la que deseas unirte."}
+          inputs={[{ placeholder: "Codigo", type: "text", value: code, onChange: (e) => setCode(e.target.value) }] }
+          confirmButtonText={"Enviar"}
+          cancelButtonText={"Cancelar"} 
+          onCancel={() =>{ setShowDialog(false); setCode('')}} 
+          onConfirm={handleJoin}
+          />
+      }
+      </div>
   </div>
   );
 };
