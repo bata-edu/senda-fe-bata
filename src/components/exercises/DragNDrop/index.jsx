962de2959255df924aa74f3806b37c886a642b8f
Ezequiel Otero treacey <ezequieloterotreacey@192.168.1.4>
2024-12-26 12:00:21 -0300

    feat: exercises

---
+++ src/components/exercises/DragNDrop/index.jsx
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    id: 1,
+    content: "Aqua-man",
+    status: "good",
+  },
+  {
+    id: 2,
+    content: "Flash",
+    status: "normal",
+  },
+  {
+    id: 3,
+    content: "Green Lantern",
+    status: "good",
+  },
+  {
+    id: 4,
+    content: "Batman",
+    status: "bad",
+  },
+];
