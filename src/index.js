66786fdd431ce56e912a33b3949b1710ea9b45f3
julianlinaress <julian@linares.com.ar>
2025-02-28 01:16:40 -0300

    fix: bootstrap import

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import store from "./app/store";
 import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { MantineProvider } from "@mantine/core";
 const root = ReactDOM.createRoot(document.getElementById("root"));
